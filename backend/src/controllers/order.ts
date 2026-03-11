f6f8c2063f1d0316484a11daee0fafcf5bf16e98
Dmitry Kostylev <smileyfvce@yandex.ru>
2026-03-12 01:44:00 +0300

    v1.0

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,16 +1,22 @@
-import { Request, Response, NextFunction } from 'express';
-import { faker } from '@faker-js/faker';
-import Product from '../models/product';
+import { Request, Response, NextFunction } from "express";
+import { faker } from "@faker-js/faker";
+import Product from "../models/product";
 
 async function createOrder(req: Request, res: Response, next: NextFunction) {
   try {
-    const { items } = req.body;
+    const { items, payment, total } = req.body;
 
     const products = await Product.find({ _id: { $in: items } });
 
+    // проверка корректного способа оплаты
+    if (payment != "card" && payment != "online") {
+      res.status(400).json({ message: "Некорректный способ оплаты" });
+      return;
+    }
+
     // проверка: существования товаров в бд
     if (products.length !== items.length) {
-      res.status(400).json({ message: 'Товар не существует' });
+      res.status(400).json({ message: "Товар не существует" });
       return;
     }
 
@@ -19,7 +25,7 @@ async function createOrder(req: Request, res: Response, next: NextFunction) {
       (product) => product.price === null,
     );
     if (nullPriceProduct.length > 0) {
-      res.status(400).json({ message: 'Товар не продаётся' });
+      res.status(400).json({ message: "Товар не продаётся" });
       return;
     }
     // проверка суммы
@@ -27,7 +33,10 @@ async function createOrder(req: Request, res: Response, next: NextFunction) {
       (sum, product) => sum + (product.price || 0),
       0,
     );
-
+    if (totalSum !== total) {
+      res.status(400).json({ message: "Сумма заказа не совпадает" });
+      return;
+    }
     const order = {
       id: faker.string.uuid(),
       total: totalSum,
